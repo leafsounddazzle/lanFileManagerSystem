f9e53599f035271a115fd2523db7c27e9153f9f6
leafsounddazzle <leafsounddazzle@hotmail.com>
2017-06-08 17:39:55 +0800

    2017.6.7

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() {
+export class Login {
+  public form:FormGroup;
+  public email:AbstractControl;
+  public password:AbstractControl;
+  public submitted:boolean = false;
+
+  constructor(fb:FormBuilder) {
+    this.form = fb.group({
+      'email': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+    });
+
+    this.email = this.form.controls['email'];
+    this.password = this.form.controls['password'];
   }
 
-  ngOnInit() {
+  public onSubmit(values:Object):void {
+    this.submitted = true;
+    if (this.form.valid) {
+      // your code goes here
+      // console.log(values);
+    }
   }
-
 }
