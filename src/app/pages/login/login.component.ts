990db4192b7cad15350275f3a4436ef0f891508d
leafsounddazzle <leafsounddazzle@hotmail.com>
2017-06-28 17:54:34 +0800

    .

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
