2880f5eb87cd3294ae0da12d995fc372fd92bd8a
leafsounddazzle <leafsounddazzle@hotmail.com>
2017-09-20 17:14:32 +0800

    Move menu structure to single file.

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TreeModule, TreeNode} from "angular-tree-component"
+import { PAGES_MENU } from "../pages.menu"
 import * as $ from 'jquery';
 
 
@@ -15,46 +16,7 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.nodes = [
-      {
-        id: 1,
-        name: '文件',
-        alias: 'file',
-        icon: 'folder',
-        children: [
-          { id: 2, name: 'child1' },
-          { id: 3, name: 'child2' }
-        ]
-      },
-      {
-        id: 4,
-        name: '最近访问',
-        alias: 'recents',
-        icon: 'history',
-        children: [
-          { id: 5, name: 'child2.1' },
-          {
-            id: 6,
-            name: 'child2.2',
-            children: [
-              { id: 7, name: 'subsub' }
-            ]
-          }
-        ]
-      },
-      {
-        id: 8,
-        name: '共享',
-        alias: "share",
-        icon: "share"
-      },
-      {
-        id: 9,
-        name: '关于',
-        alias: "about",
-        icon: "help"
-      }
-    ];
+    this.nodes = PAGES_MENU;
   }
 
   go($event) {
--- src/app/pages/pages.menu.ts
+++ src/app/pages/pages.menu.ts
@@ -1,19 +1,40 @@
 export const PAGES_MENU = [
   {
-    path: 'pages',
+    id: 1,
+    name: '文件',
+    alias: 'file',
+    icon: 'folder',
     children: [
+      { id: 2, name: 'child1' },
+      { id: 3, name: 'child2' }
+    ]
+  },
+  {
+    id: 4,
+    name: '最近访问',
+    alias: 'recents',
+    icon: 'history',
+    children: [
+      { id: 5, name: 'child2.1' },
       {
-        path: 'dashboard',
-        data: {
-          menu: {
-            title: 'general.menu.dashboard',
-            icon: 'ion-android-home',
-            selected: false,
-            expanded: false,
-            order: 0
-          }
-        }
+        id: 6,
+        name: 'child2.2',
+        children: [
+          { id: 7, name: 'subsub' }
+        ]
       }
     ]
+  },
+  {
+    id: 8,
+    name: '共享',
+    alias: "share",
+    icon: "share"
+  },
+  {
+    id: 9,
+    name: '关于',
+    alias: "about",
+    icon: "help"
   }
 ];
